aeab624dc4f24632dfbc569f83bd6b08a4a3c11e
HEHLEN Léo <hehlen.l@paris.sfeir.com>
2021-06-15 10:30:08 +0200

    corrected python scripts

--- fill_db_dsdlv/init_db.py
+++ fill_db_dsdlv/init_db.py
@@ -1,29 +1,8 @@
 from suscriber import Subscriber
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-
 # FULL_DB_URL = "mysql://b336c0cb38e7c5:97184676@eu-cdbr-west-01.cleardb.com/heroku_e4bee02313f3859"
-
-host = "vps-f7b5701d.vps.ovh.net"
-user = "root"
-password = ""
-database = "dusoleil"
-uri = "mysql+pymysql://" + user + ":" + password + "@" + host + "/" + database
-
-app.config['MYSQL_HOST'] = host
-app.config['MYSQL_USER'] = user
-app.config['MYSQL_PASSWORD'] = password
-app.config['MYSQL_DB'] = database
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
-
-db = SQLAlchemy(app)
 
 s = Subscriber()
 
 r = s.query.all()
 
-print(r)
+print(r.__len__())
--- fill_db_dsdlv/main.py
+++ fill_db_dsdlv/main.py
@@ -3,16 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'admin'
-app.config['MYSQL_PASSWORD'] = 'admin'
-app.config['MYSQL_DB'] = 'dusoleil'
+host = "vps-f7b5701d.vps.ovh.net"
+user = "root"
+password = ""
+database = "dusoleil"
+uri = "mysql+pymysql://" + user + ":" + password + "@" + host + "/" + database
+
+app.config['MYSQL_HOST'] = host
+app.config['MYSQL_USER'] = user
+app.config['MYSQL_PASSWORD'] = password
+app.config['MYSQL_DB'] = database
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin@localhost/dusoleil'
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
 db = SQLAlchemy(app)
-
 
 # if __name__ == '__main__':
 #     app.run()
